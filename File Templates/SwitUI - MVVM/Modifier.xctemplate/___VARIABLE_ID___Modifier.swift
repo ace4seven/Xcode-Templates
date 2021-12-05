fba3b82649cbe174d5db0489a4307fdec46ecee4
Juraj Macák <jurajmacak@gmail.com>
2021-12-05 11:51:38 +0100

    fix: Fixed modifier creation

--- File Templates/SwitUI - MVVM/Modifier.xctemplate/___VARIABLE_ID___Modifier.swift
+++ File Templates/SwitUI - MVVM/Modifier.xctemplate/___VARIABLE_ID___Modifier.swift
@@ -13,7 +13,7 @@ struct ___VARIABLE_ID___Modifier: ViewModifier {
 
 // MARk: - Private
 
-extension ___VARIABLE_ID___View {
+extension ___VARIABLE_ID___Modifier {
 
 }
 
@@ -31,12 +31,7 @@ private struct ___VARIABLE_ID___Modifier_Previews: PreviewProvider {
 
     static var previews: some View {
         Group {
-            ___VARIABLE_ID___Modifier()
-                .previewLayout(.sizeThatFits)
-                .preferredColorScheme(.light)
-            ___VARIABLE_ID___Modifier()
-                .previewLayout(.sizeThatFits)
-                .preferredColorScheme(.dark)
+
         }
     }
     
